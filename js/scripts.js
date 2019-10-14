8c44e83a21ce49c4d1876fbd48f466c3089330f7
in24 <54269634+in24@users.noreply.github.com>
2019-10-14 12:49:20 +0300

    Uztaisīju HTML un CSS līdz galam

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
 function atteloHTML() {
   //  Paņem nolasīto jautājumu un 4 atbildes un ieliek to HTML īstajās vietās, izmantojot flexbox
 }
+function nakamais() {
+  // Pēc atbildes nospiešanas uztaisa pauzi, parāda infomatīvu logu, ka atbilde pieņemta
+}
