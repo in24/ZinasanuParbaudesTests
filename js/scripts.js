66c0d77809d12d3782de8bd824e20d8bdef6010b
in24 <54269634+in24@users.noreply.github.com>
2019-10-14 13:57:27 +0300

    Pievienoju skripta sākumu

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,16 @@
 function atteloHTML() {
-  //  Paņem nolasīto jautājumu un 4 atbildes un ieliek to HTML īstajās vietās, izmantojot flexbox
+  /* Funkcija paņem nolasīto jautājumu un 4 atbildes
+   * un ieliek to HTML īstajās vietās,
+   * izmantojot flexbox */
+  // Pagaidu variants: vienkārši piešķīru vērtības
+  let jautajums = "Kāda uzdevuma veikšanai paredzēta HTML valoda?";
+  let pareiza = "Tīmekļa lapas satura un struktūras aprakstam";
+  let atbildes = [
+    "Tīmekļa lapas noformējuma aprakstam",
+    "Tīmekļa lapas satura drošai pārsūtīšanai tīklā",
+    "Tīmekļa lapas programmēšanai",
+    "Tīmekļa lapas satura un struktūras aprakstam"
+  ];
 }
 function nakamais() {
   // Pēc atbildes nospiešanas uztaisa pauzi, parāda infomatīvu logu, ka atbilde pieņemta
