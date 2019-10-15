a91601f90c2d04ca76502728f396b7f9bd652b9c
juris124 <31391698+juris124@users.noreply.github.com>
2019-10-15 19:15:37 +0300

    Juris - testa jautājumi pagaidām kā mainīgais

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,15 @@ function atteloHTML() {
   /* Funkcija paņem nolasīto jautājumu un 4 atbildes
    * un ieliek to HTML īstajās vietās,
    * izmantojot flexbox */
+
+  // pagaidu variants - piešķīru vērtības kā objektam ar atbilžu masīvu, lai var turpināt pārējās funkcijas (Juris)
+  let testaJaut = [
+    {'jautajums': "Kāda jūra apskalo Latvijas krastu?", 'pareiza': "Baltijas jūra", 'atbildes': ["Rīgas jūra", "Ziemeļu jūra","Melnā jūra", "Baltijas jūra"]},
+    {'jautajums': "Kura pilsēta ir Latvijas galvaspilsēta?", 'pareiza': "Rīga", 'atbildes': ["Rīga", "Daugavpils", "Ventspils", "Ogre"]},
+    {'jautajums': "Kurā Latvijas novadā atrodas Atpūtas ciems?", 'pareiza': "Zemgalē", 'atbildes': ["Latgalē", "Zemgalē","Kurzemē", "Vidzemē"]}
+    ];
   // Pagaidu variants: vienkārši piešķīru vērtības
+    /*
   let jautaajums = "Mans jautājums";
   let pareiza = "Tīmekļa lapas satura un struktūras aprakstam";
   let atbildes = [
@@ -12,12 +20,15 @@ function atteloHTML() {
     "Tīmekļa lapas programmēšanai",
     "Tīmekļa lapas satura un struktūras aprakstam"
   ];
+*/
+
   // Nomaina jautājumu
-  document.getElementById("jaut").innerText = jautaajums;
+  let j=0; // pagaidu mainīgais testa tekošā jautājuma definēšanai, pagaidām ir 3 jautājumi 0,1,2
+  document.getElementById("jaut").innerText = testaJaut[j].jautajums;  
   // Nomaina atbilžu pogas
   let atbilzuTeksti = document.getElementsByClassName("atb");
-  for (let i = 0; i < atbilzuTeksti.length; i++) {
-    atbilzuTeksti[i].value = atbildes[i];
+  for (let i = 0; i < testaJaut[j].atbildes.length; i++) {
+    atbilzuTeksti[i].value = testaJaut[j].atbildes[i];
   }
 }
 function nakamais() {
