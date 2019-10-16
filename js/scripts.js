1c9eb3be1a3ac1551c843bdb92c69d923f723776
Gundega Dekena <gundega@spacetree.cloud>
2019-10-17 00:06:48 +0300

    pievienots paarejas atteels

--- js/scripts.js
+++ js/scripts.js
@@ -1,45 +1,32 @@
 /* Funkcija paņem nolasīto jautājumu un 4 atbildes
-   * un ieliek to HTML īstajās vietās,
-   * izmantojot flexbox */
+ * un ieliek to HTML īstajās vietās,
+ * izmantojot flexbox */
 var testaJautajumi;
+var preloader;
 
-function atteloHTML() {  
-// pagaidu variants - piešķīru vērtības kā objektam ar atbilžu masīvu, lai var turpināt pārējās funkcijas (Juris)
-/*let testaJautajumi = [
-  {'jautajums': "Kāda jūra apskalo Latvijas krastu?", 'pareiza': "Baltijas jūra", 'atbildes': ["Rīgas jūra", "Ziemeļu jūra","Melnā jūra", "Baltijas jūra"]},
-  {'jautajums': "Kura pilsēta ir Latvijas galvaspilsēta?", 'pareiza': "Rīga", 'atbildes': ["Rīga", "Daugavpils", "Ventspils", "Ogre"]},
-  {'jautajums': "Kurā Latvijas novadā atrodas Atpūtas ciems?", 'pareiza': "Zemgalē", 'atbildes': ["Latgalē", "Zemgalē","Kurzemē", "Vidzemē"]}
-  ];
-
-// Pagaidu variants: vienkārši piešķīru vērtības (Indra)  
-let jautaajums = "Mans jautājums";
-let pareiza = "Tīmekļa lapas satura un struktūras aprakstam";
-let atbildes = [
-  "Tīmekļa lapas noformējuma aprakstam",
-  "Tīmekļa lapas satura drošai pārsūtīšanai tīklā",
-  "Tīmekļa lapas programmēšanai",
-  "Tīmekļa lapas satura un struktūras aprakstam"
-];
-*/ 
-
-testaJautajumiNoCSV(function(results){
- testaJautajumi=results.data.map(function(csvJautajums) {
+function atteloHTML() {
+  testaJautajumiNoCSV(function(results) {
+    testaJautajumi = results.data.map(function(csvJautajums) {
       return {
-        'jautajums': csvJautajums.jautajums,
-        'pareiza': csvJautajums.pareiza,
-        'atbildes': [csvJautajums.atbilde1, csvJautajums.atbilde2, csvJautajums.atbilde3, csvJautajums.atbilde4]
-      }
+        jautajums: csvJautajums.jautajums,
+        pareiza: csvJautajums.pareiza,
+        atbildes: [
+          csvJautajums.atbilde1,
+          csvJautajums.atbilde2,
+          csvJautajums.atbilde3,
+          csvJautajums.atbilde4
+        ]
+      };
     });
-console.log(testaJautajumi);
-nomainitJautajumu(testaJautajumi);
-});
-
+    console.log(testaJautajumi);
+    nomainitJautajumu(testaJautajumi);
+  });
 }
 
-function nomainitJautajumu(testaJautajumi){
+function nomainitJautajumu(testaJautajumi) {
   // Nomaina jautājumu
-  let j=0; // pagaidu mainīgais testa tekošā jautājuma definēšanai, pagaidām ir 6 jautājumi 0,1, .., 5. Vēlāk šis mainīsies automātiski kad būs programmēta pāreja pie nākamā jautājuma.
-  document.getElementById("jaut").innerText = testaJautajumi[j].jautajums;  
+  let j = 0; // pagaidu mainīgais testa tekošā jautājuma definēšanai, pagaidām ir 6 jautājumi 0,1, .., 5. Vēlāk šis mainīsies automātiski kad būs programmēta pāreja pie nākamā jautājuma.
+  document.getElementById("jaut").innerText = testaJautajumi[j].jautajums;
   // Nomaina atbilžu pogas
   let atbilzuTeksti = document.getElementsByClassName("atb");
   for (let i = 0; i < testaJautajumi[j].atbildes.length; i++) {
@@ -48,6 +35,12 @@ function nomainitJautajumu(testaJautajumi){
 }
 
 function nakamais() {
+  preloader = document.querySelector(".preloader");
+  preloader.style.opacity = 1;
+  preloader.classList.remove("behind");
+  preloader.classList.add("front");
+  fadeEffect();
+  //preloader.classList.replace("front", "behind");
   // Pēc atbildes nospiešanas uztaisa pauzi, parāda infomatīvu logu, ka atbilde pieņemta
 }
 
@@ -63,4 +56,18 @@ function testaJautajumiNoCSV(callback) {
     }
   });
   console.log("CSV fails nolasīts");
+}
+
+function fadeEffect() {
+  preloader.classList.replace("behind", "front");
+  // saakas transition no opacity 0->1, 1 sekundi ilga
+  preloader.style.opacity = 1;
+  // taapeec uzliekam timeout uz 1000ms, kad saakam transition 1->0
+  setTimeout(function() {
+    preloader.style.opacity = 0;
+  }, 1000);
+  // otraa transition beidzas peec 2s, aizvaacam, lai netraucee spiest pogas
+  setTimeout(function() {
+    preloader.classList.replace("front", "behind");
+  }, 2000);
 }
